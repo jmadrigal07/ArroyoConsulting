1c5783d169e37c73d0e9771c2e04ec9040670fb9
Jorge Luis Madrigal <jmadrigal.webmaster@gmail.com>
2024-03-17 20:00:56 -0500

    Update chocolatey.ps1

--- chocolatey.ps1
+++ chocolatey.ps1
@@ -1,1 +1,5 @@
+# Install Chocolatey
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
+# Install Google Chrome using Chocolatey
+choco install googlechrome -y
