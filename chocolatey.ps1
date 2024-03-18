a93aab3445b694409ebb07bbcf74b1b7fc0dfc40
Jorge Luis Madrigal <jmadrigal.webmaster@gmail.com>
2024-03-17 23:11:27 -0500

    Update chocolatey.ps1

--- chocolatey.ps1
+++ chocolatey.ps1
@@ -3,3 +3,31 @@ Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.We
 
 # Install Google Chrome using Chocolatey
 choco install googlechrome -y
+
+# Install Office
+choco install microsoft-office -y
+
+# Install Node.js (any version)
+choco install nodejs -y
+
+# Install Java
+choco install adoptopenjdk -y
+
+# Configure Java environment variables
+[System.Environment]::SetEnvironmentVariable('JAVA_HOME', 'C:\Program Files\AdoptOpenJDK\jdk-15.0.2.7-hotspot', 'Machine')
+[System.Environment]::SetEnvironmentVariable('Path', $env:Path + ';C:\Program Files\AdoptOpenJDK\jdk-15.0.2.7-hotspot\bin', 'Machine')
+
+# Install Visual Studio Code
+choco install vscode -y
+
+# Install Git
+choco install git -y
+
+# Install Git Bash
+choco install git.install --params "/GitAndUnixToolsOnPath" -y
+
+# Print system environment variables
+Get-ChildItem Env: | Format-Table -AutoSize
+
+# Restart the VM (requires admin privileges)
+Restart-Computer -Force
